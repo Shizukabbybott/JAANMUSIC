509835436b375846bad1d742172ef9f27472ac6d
RAVAN <89262522+Shizukabbybott@users.noreply.github.com>
2022-03-12 04:02:02 -0500

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -33,25 +33,25 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/1a85ce935e13e92706907.jpg",
+        photo=f"https://telegra.ph/file/88053e21bccad73d8a956.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
 💥 ʜᴇʟʟᴏ, ɪ ᴀᴍ ɴᴏʙɪᴛᴀ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ
 ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ...
 ┏━━━━━━━━━━━━━━━━━┓
-┣★ ᴄʀᴇᴀᴛᴏʀ : [KULDIP RATHOD](https://t.me/AAPLI_YAARI)
-┣★ ᴜᴘᴅᴀᴛᴇs : [NOBITA SUPPORT](https://t.me/Nobita_shizuka_repo)
-┣★ sᴜᴘᴘᴏʀᴛ : [NOBITA CHAT](https://t.me/AAPLI_YAARI)
-┣★ ᴏᴡɴᴇʀ › : [NOBITA_XD](@Nobi_xxd)
+┣★ ᴄʀᴇᴀᴛᴏʀ : [RAVAN BABU](https://t.me/rawanbabu)
+┣★ ᴜᴘᴅᴀᴛᴇs : [ASU SUPPORT](https://t.me/ravanbots)
+┣★ sᴜᴘᴘᴏʀᴛ : [ASU CHAT](https://t.me/bestfriendfoverever)
+┣★ ᴏᴡɴᴇʀ › : [RAVANBABU](@rawanbabu)
 ┗━━━━━━━━━━━━━━━━━┛
 
 💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ
-ᴅᴍ ᴛᴏ ᴍʏ [ʟᴇɢᴇɴᴅ ᴏᴡɴᴇʀ](https://t.me/AAPLI_YAARI) ...
+ᴅᴍ ᴛᴏ ᴍʏ [ʟᴇɢᴇɴᴅ ᴏᴡɴᴇʀ](https://t.me/rawanbabu) ...
 ━━━━━━━━━━━━━━━━━━━━━━━━**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", url=f"https://t.me/AAPLI_YAARI")
+                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", url=f"https://t.me/ravanbots")
                 ]
                 
            ]
@@ -59,16 +59,16 @@ async def start_(client: Client, message: Message):
     )
     
     
-@Client.on_message(commandpro(["/start", "/alive", "NOBITA"]) & filters.group & ~filters.edited)
+@Client.on_message(commandpro(["/start", "/alive", "ASU"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/1a85ce935e13e92706907.jpg",
+        photo=f"https://telegra.ph/file/88053e21bccad73d8a956.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ ᴀɴᴅ sᴜᴘᴘᴏʀᴛ 💞", url=f"https://t.me/AAPLI_YAARI")
+                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ ᴀɴᴅ sᴜᴘᴘᴏʀᴛ 💞", url=f"https://t.me/bestfriendfoverever")
                 ]
             ]
         ),
@@ -78,7 +78,7 @@ async def start(client: Client, message: Message):
 @Client.on_message(commandpro(["repo", "#repo", "@repo", "/repo", "source"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/1a85ce935e13e92706907.jpg",
+        photo=f"https://telegra.ph/file/88053e21bccad73d8a956.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
